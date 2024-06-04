0167c398bb8bd07b7838d8aec1cbc468978d04a3
juli <yuuumereles@gmail.com>
2024-06-04 16:01:48 -0300

    agregue cambios

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Link, Stack } from 'expo-router';
+import { View, Text, Image, Button, StyleSheet, ScrollVi } from 'react-native';
+
+const IndexScreen=()=>{
+  return(
+    <>
+    <View style={styles.container}>
+
+    </View>
+    </>
+  )
+}
